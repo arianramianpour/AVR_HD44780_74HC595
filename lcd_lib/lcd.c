a1d69bd300f803cae4880afe074673d9d6621e8d
arianramianpour <44116550+arianramianpour@users.noreply.github.com>
2025-05-13 22:13:53 +0430

    lcd init
    starting the lcd and changing it to 4 bit mode

--- lcd_lib/lcd.c
+++ lcd_lib/lcd.c
@@ -1,21 +1,75 @@
-#ifndef _LCD_H
-#define _LCD_H
+#include <mega32.h>
+#include <delay.h>
+#include "lcd.h"
 
+void lcd_send_E(unsigned char data)   {     //simple 4 bit send to lcd pins
+    unsigned char myport = 0;
+    myport = ((data & 0x0f) << 3)   ;  
+    
+    myport = myport | 0x04 ; 
+    send_ports(myport);
+    delay_us(5);
+    myport = myport & ~(0x04) ;             //The lcd will read its pins after turning off the E pin
+    send_ports(myport); 
+    delay_us(5);
+     
+}
 
-#define LCD_PORT PORTA
-#define LCD_DS 0
-#define LCD_SH_CP   1
-#define LCD_ST_CP   2
+void lcd_send_E_8(unsigned char data)   {  //simple 8 bit send to lcd pins 
+    unsigned char myport = 0;
+    myport = (((data >> 4 ) & 0x0f) << 3)   ;  
+    
+    myport = myport | 0x04 ;
+    send_ports(myport); 
+    delay_ms(1);  
+    myport = myport & ~(0x04) ; 
+    send_ports(myport);
+    delay_ms(1);
+    
+    myport = ((data  & 0x0f) << 3)   ;  
+    
+    myport = myport | 0x04 ;
+    send_ports(myport);
+    delay_ms(1);
+    myport = myport & ~(0x04) ;
+    send_ports(myport); 
+    delay_ms(1);
+     
+}
 
-#pragma used+
+void lcd_send_ERS_8(unsigned char data)   { //simple 8 bit send to lcd pins with RS
+    unsigned char myport = 0;
 
+    myport = (((data >> 4 ) & 0x0f) << 3) |  0x01 ;  
+    
+    myport = myport | 0x04 ; 
+    send_ports(myport);
+    delay_ms(1);
+    myport = myport & ~(0x04) ;
+    send_ports(myport); 
+    delay_ms(1);
+    
+    myport = ((data  & 0x0f) << 3) | 0x01   ;  
+    
+    myport = myport | 0x04 ; 
+    send_ports(myport);
+    delay_ms(1);
+    myport = myport & ~(0x04) ; 
+    send_ports(myport);
+    delay_ms(1);
+     
+}
 
-void lcd_init();
-void lcd_put_char(char c);
-void lcd_puts(char *c);
-void lcd_clear();
-void lcd_gotoxy(char x , char y);
+void lcd_init(){                           // just like datasheet
+    LCD_DDR |= (1 <<LCD_DS) | (1 <<LCD_SH_CP) | (1 <<LCD_ST_CP)
 
+    lcd_send_E(0x03);
+    lcd_send_E(0x03);
+    lcd_send_E(0x03);
+    lcd_send_E(0x02);   //change to 4 bit mode 
 
-#pragma used-
-#endif
+    lcd_send_E_8(0x28);
+    lcd_send_E_8(0x0C);
+    lcd_send_E_8(0x06);
+        
+}  
