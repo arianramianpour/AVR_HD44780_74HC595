1d4bd3e782a2889623ae36253b41f12ab298f354
arianramianpour <44116550+arianramianpour@users.noreply.github.com>
2025-05-13 22:19:58 +0430

    adding put char & puts
    adding putchar and puts

--- lcd_lib/lcd.c
+++ lcd_lib/lcd.c
@@ -8,10 +8,10 @@ void lcd_send_E(unsigned char data)   {     //simple 4 bit send to lcd pins
     
     myport = myport | 0x04 ; 
     send_ports(myport);
-    delay_us(5);
+    delay_ms(1);
     myport = myport & ~(0x04) ;             //The lcd will read its pins after turning off the E pin
     send_ports(myport); 
-    delay_us(5);
+    delay_ms(1);
      
 }
 
@@ -37,7 +37,7 @@ void lcd_send_E_8(unsigned char data)   {  //simple 8 bit send to lcd pins
      
 }
 
-void lcd_send_ERS_8(unsigned char data)   { //simple 8 bit send to lcd pins with RS
+void lcd_send_ERS_8(unsigned char data)   { //simple 8 bit send to lcd pins with RS , this is only needed for writing something on lcd
     unsigned char myport = 0;
 
     myport = (((data >> 4 ) & 0x0f) << 3) |  0x01 ;  
@@ -73,3 +73,16 @@ void lcd_init(){                           // just like datasheet
     lcd_send_E_8(0x06);
         
 }  
+
+void lcd_put_char(char c) {     //simple 1 character send to lcd
+    lcd_send_ERS_8(c);          //as datasheet said , it should be same as asci table 
+
+}
+
+
+void lcd_puts(char *c) {        //simple string send to lcd
+    while(*c != 0){             //this will stuck on loop until it hits null
+    lcd_put_char(*c);           //sending every characters one by one to lcd
+    c++;                        
+    }
+}
