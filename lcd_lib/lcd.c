fb6a7fe498d40c66c375f97482ee725eeee490a2
arianramianpour <44116550+arianramianpour@users.noreply.github.com>
2025-05-15 11:32:36 +0430

    adding send ports for 74hc595

--- lcd_lib/lcd.c
+++ lcd_lib/lcd.c
@@ -2,6 +2,23 @@
 #include <delay.h>
 #include "lcd.h"
 
+
+void send_ports(unsigned char a){
+  unsigned char i = 0;
+    for(i = 0; i < 8 ; i ++){
+        if((a << i)&0x80)
+            LCD_PORT |= (1 << LCD_DS) | (1 << LCD_SH_CP);
+        else     
+        LCD_PORT |= (1 << LCD_DS) ;
+         
+        LCD_PORT &= ~((1 << LCD_DS) | (1 << LCD_SH_CP)); 
+         
+            } 
+            
+    LCD_PORT |= (1 << LCD_ST_CP) ;
+    LCD_PORT &= ~(1 << LCD_ST_CP);
+}
+
 void lcd_send_E(unsigned char data)   {     //simple 4 bit send to lcd pins
     unsigned char myport = 0;
     myport = ((data & 0x0f) << 3)   ;  
@@ -61,7 +78,7 @@ void lcd_send_ERS_8(unsigned char data)   { //simple 8 bit send to lcd pins with
 }
 
 void lcd_init(){                           // just like datasheet
-    LCD_DDR |= (1 <<LCD_DS) | (1 <<LCD_SH_CP) | (1 <<LCD_ST_CP)
+    LCD_DDR |= (1 <<LCD_DS) | (1 <<LCD_SH_CP) | (1 <<LCD_ST_CP);
 
     lcd_send_E(0x03);
     lcd_send_E(0x03);
